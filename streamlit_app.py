22734c5b5e70cd95f97acdd1bdbaf2a15a668d88
Suren Tekkem <59258808+stekkem@users.noreply.github.com>
2022-05-13 12:44:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header(':plate Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(':_badger: Breakfast Menu')
+streamlit.text(':_waving: Omega 3 & Blueberry Oatmeal')
+streamlit.text(':_yesssss: Kale, Spinach & Rocket Smoothie')
+streamlit.text(':_bird: Hard-Boiled Free-Range Egg')
