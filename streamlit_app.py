b3c2405f6f907d5a00439ee5b8ebadf406d6583b
Suren Tekkem <59258808+stekkem@users.noreply.github.com>
2022-05-13 12:48:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header(':_badger: Breakfast Menu')
-streamlit.text(':_waving: Omega 3 & Blueberry Oatmeal')
-streamlit.text(':_yesssss: Kale, Spinach & Rocket Smoothie')
-streamlit.text(':_bird: Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Favorites')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
