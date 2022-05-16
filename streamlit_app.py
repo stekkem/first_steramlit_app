db43113d906717d68ae74aa07bee49b547494ce2
Suren Tekkem <59258808+stekkem@users.noreply.github.com>
2022-05-16 13:44:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,10 @@ streamlit.header('Fruityvice Fruit Advice')
 
 import requests
 
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choise=streamlit.text_input('What fruit would you like information about?', 'kiwi')
+streamlit.write('The user entered ', fruit_choise)
+
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choise)
 #streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
